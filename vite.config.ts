83c1ca880c7c4f15bff164ce0e8fc0f907fea40e
alexlin8888 <153320559+alexlin8888@users.noreply.github.com>
2025-09-30 14:52:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vite/client" />
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string
+  readonly VITE_OPENAQ_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
